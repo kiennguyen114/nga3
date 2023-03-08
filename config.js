e0d7a074d727da081d3dc1fbe923f2b6726c153f
kiennguyen114 <116872111+kiennguyen114@users.noreply.github.com>
2023-03-08 14:21:31 +0700

    main

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'https://kiennguyen114.github.io/thu8-3/';
 const eventName = 'Chúc Mừng mùng 8-3';
 const titleCard = 'Tặng nga';
-const contentCard = 'Nụ cười em đánh rơi <br> Anh vô tình nhặt lấy <br>Kể từ ngày hôm ấy <br> Anh thành kẻ tương tư <br> Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = ' Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 
 const giftImage = 'nag.jpeg';
