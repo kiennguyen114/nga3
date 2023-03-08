b0ca63fc05ff822a208ad20392d0714ea6fd76b8
kiennguyen114 <116872111+kiennguyen114@users.noreply.github.com>
2023-03-08 13:26:32 +0700

    main

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://kiennguyen114.github.io/thu8-3/';
+const eventName = 'Chúc Mừng mùng 8-3';
+const titleCard = 'Tặng nga';
+const contentCard = 'Nụ cười em đánh rơi <br> Anh vô tình nhặt lấy <br>Kể từ ngày hôm ấy <br> Anh thành kẻ tương tư <br> Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'nag.jpeg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
